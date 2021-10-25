d85d93463a3a16251330cbefab6a89e6b0c0f04d
latenightluke <luke.ivie@icloud.com>
2021-10-25 09:37:06 -0700

    Fix: error when importing logo in header, remove logo

--- pages/pages/header/Logo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Image from "next/image";
-import lnlLogo from "../../public/lnl.svg";
-
-export default function Logo() {
-  return <Image src={lnlLogo} alt="LNL" />;
-}
