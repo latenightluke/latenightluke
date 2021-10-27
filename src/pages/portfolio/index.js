d8ffd40ca0c470491bfe7a71fa488edeff9b7f9f
latenightluke <luke.ivie@icloud.com>
2021-10-26 22:29:18 -0700

    Fix: update page titles for Blog, Tools, and Portfolio

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Head from "next/head";
 import Layout from "../../components/layout";
 
 export default function index() {
   return (
     <Layout>
+      <Head>
+        <title>Portfolio | Late Night Luke</title>
+      </Head>
       <div className="flex flex-col text-center max-w-screen-xl mx-auto p-6 text-white">
         <h1 className="text-3xl font-bold mb-1">Portfolio</h1>
         <p className="text-white dark:text-indigo-400 text-xl">Coming Soon</p>
